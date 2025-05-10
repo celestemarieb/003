7e23eeda7ddc7ca9088b8b6802e0327082ddaf78
celestemarieb <34022835+celestemarieb@users.noreply.github.com>
2025-05-10 18:38:33 +1000

    add storage for posts and set-up files

---
+++ script.js
@@ -0,0 +1,16 @@
+fetch('posts.json')
+    .then(response => response.json())
+    .then(posts => {
+        const container = document.getElementById('post-list');
+        posts.reverse().forEach(post => {
+            const article = document.createElement('article');
+            article.innerHTML = `
+                <p>${post.content}</p>
+                <p>${post.date}</p>
+                `;
+            container.appendChild(article);
+        });
+    })
+    .catch(error => {
+        console.error('Failed to load posts:',error);
+    });
