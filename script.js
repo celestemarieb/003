9e9c64627e986dad7f65187529b7e2c71c4f4ca0
celestemarieb <34022835+celestemarieb@users.noreply.github.com>
2025-06-11 13:33:59 +1000

    initial commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ fetch('posts.json')
     .then(response => response.json())
     .then(posts => {
         const container = document.getElementById('post-list');
-        posts.reverse().forEach(post => {
+        posts.forEach(post => {
             const article = document.createElement('article');
             article.innerHTML = `
                 <p>${post.content}</p>
